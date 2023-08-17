ebe8f10ec4e0dffa3e31d6527dead727219b5c2b
Nikk45 <nicksin36@gmail.com>
2023-08-17 16:20:02 +0530

    submit3

--- src/components/Child.js
+++ src/components/Child.js
@@ -1,11 +1,28 @@
-import { divide } from 'cypress/types/lodash'
-import React from 'react'
+import { logDOM } from '@testing-library/react'
+import { set } from 'lodash';
+import React, { useState } from 'react'
 
-const Child = ()=>{
+const Child = ({setShowModal})=>{
+    
+    let [clicked, setClicked] = useState(false);
+    // console.log(typeof setShowModal);
+
+    const update = ()=>{
+        setShowModal("true")
+        if(clicked === false)
+            setClicked(true)
+        else
+            setClicked(false)
+    }
+
     return(
         <div className='child'>
             <h2>Child Component</h2>
-            <button>Show Modal</button>
+            <button onClick={update}>Show Modal</button>
+            <div className={ clicked === true ? 'active': 'modal'}>
+                <h3>Modal Content</h3>
+                <p>This is the modal content.</p>
+            </div>
         </div>
     )
 }
--- src/components/Parent.js
+++ src/components/Parent.js
@@ -1,15 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Child from './Child'
 
 const Parent = ()=>{
+
+    const [showModal, setShowModal] = useState('false')
+
     return(
         <div className='parent'>
             <h1>Parent Component</h1>
-            <Child />
-            <div className='modal'>
-                <h3>Modal Content</h3>
-                <p>This is the modal content.</p>
-            </div>
+            <Child setShowModal = {setShowModal}/>
         </div>
     )
 }
